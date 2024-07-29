cd30d12b68088b90f07ce7afca2eb8b52a118b38
vedant-80 <rsb.vedant@gmail.com>
2024-07-28 20:56:12 -0500

    Created dimension change functionality, fixed clearCanvas

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const canvas = document.querySelector('#game-container');
 const clearBtn = document.querySelector('.clear-canvas');
-
+const slider = document.querySelector('.slider');
+var gridElements = document.querySelectorAll('.col');
+const colorChoice = document.querySelector('#color-select')
 /*
 How to create the canvas grid:
 
@@ -12,35 +14,68 @@ Within every row, it will have columns with the .item class. Each item will be s
 */
 
 let canvasDimension = 475;
-let gridSize = 16;
+let gridSize = document.getElementsByClassName('.slider').value;
 
-//creates grid
-for(let i = 0; i < gridSize; i++){
-    const row = document.createElement('div');
-    row.classList.add('row');
-    canvas.appendChild(row);
-    for (let j = 0; j < gridSize; j++){
-        const col = document.createElement('div');
-        col.style.height = (canvasDimension/gridSize) + 'px';
-        col.classList.add('col');
-        row.appendChild(col);
-    }
-}
+//Creates new grid whenever dimension slider value changes
+slider.addEventListener('change', () => createGrid(Number(slider.value)));
 
-//Colors grid when mouse hovers over
-const gridElements = document.querySelectorAll('.col');
-
-for (let i = 0; i < gridElements.length; i++){
-    let element = gridElements[i];
-    element.addEventListener('mouseover', () => markBlack(element));
-}
-
-
-//Clear grid when click clear canvas btn
+//Clears canvas on click
 clearBtn.addEventListener('click', clearCanvas);
+createGrid(16);
+
+
+
+
+
+
+function createGrid(gridSize){
+    /*
+    Function: createGrid
+
+    Input:
+        - gridSize: a Number that describes how many grid blocks make up each axis
+
+    Output:
+        - deletes the previous existing grid and creates a new grid that marks grid blocks that are hovered over
+        - void return type
+    */
+
+    //Deletes grid
+    gridElements = document.querySelectorAll('.col');
+    if(gridElements.length > 0){
+        deleteCanvas();
+    }
+    //Creates grid by row and column
+    for(let i = 0; i < gridSize; i++){
+        const row = document.createElement('div');
+        row.classList.add('row');
+        canvas.appendChild(row);
+        for (let j = 0; j < gridSize; j++){
+            const col = document.createElement('div');
+            col.style.height = (canvasDimension/gridSize) + 'px';
+            col.classList.add('col');
+            //Adds marking capability when mouse goes over a grid block
+            col.addEventListener('mouseover', () => markBlack(col));
+            row.appendChild(col);
+        }
+    }
+    
+}
+//Initializes the grid
+
 
 
 function clearCanvas(){
+    /*
+    Function: clearCanvas
+
+    Input: None
+
+    Output:
+        - Changes style of every grid block to have a white background
+        - Void return type
+    */
+    gridElements = document.querySelectorAll('.col');
     for (let i = 0; i < gridElements.length; i++){
         let element = gridElements[i];
         element.style.backgroundColor = 'white';
@@ -57,4 +92,24 @@ function randomizeColor(){
     let green = Math.round(Math.random()*255);
     let blue = Math.round(Math.random()*255);
     return [red, green, blue];
+}
+
+function deleteCanvas(){
+    /*
+    Function: deleteCanvas
+
+    Inputs: None
+
+    Output: 
+        - deletes every row and column from HTML
+        - Void return type
+    */
+    for (let i = 0; i < gridElements.length; i++){
+        let element = gridElements[i];
+        element.remove();
+    }
+    let rows = document.querySelectorAll('.row');
+    rows.forEach(element => {
+        element.remove();
+    });
 }
